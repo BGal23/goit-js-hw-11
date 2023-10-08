dde2f8e32c9f717fa1365ab42bd753604aa5a437
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-10-08 14:06:59 +0200

    save

--- src/js/api.js
+++ src/js/api.js
@@ -14,5 +14,4 @@ const getPhotos = (input) => {
     })
 }
 
-
 export {getPhotos}
--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,20 @@
 import {getPhotos} from "./api.js"
 
-const input = document.querySelector("input").value 
-const searchBtn = document.querySelector("button")
-
-if (input) {
-getPhotos(input)
-    .then (data => {
-        
-        console.log(data, input === true)
-    })
-    .catch (error => {
-        console.log(error)
-    })
-}
+const searchBtn = document.querySelector("button");
 
 const createGallery = (event) => {
     event.preventDefault()
-    console.log(event)
+    const input = document.querySelector("input").value;
+
+    getPhotos(input)
+    .then (data => {
+        console.log(data)
+            return data
+        })
+        .catch (error => {
+            console.log(error)
+        })
+    
 }
 
 searchBtn.addEventListener("click", createGallery)
