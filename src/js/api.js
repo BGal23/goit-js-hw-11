3947179bc7020feeac19e4df00a728a9d54ff0fd
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-10-08 15:05:07 +0200

    save photos

--- src/js/api.js
+++ src/js/api.js
@@ -2,13 +2,9 @@ import axios from "axios";
 
 const key = "39891007-90054c7bf84e45a363315c2ae"
 
-let options = {
-    image_type: "photo"
-}
-
 const getPhotos = (input) => {
     return axios
-    .get(`https://pixabay.com/api/?key=${key}&q=${input}`, options)
+    .get(`https://pixabay.com/api/?key=${key}&q=${input}&image_type=photo&orientation=horizontal&safesearch=true`)
     .then(respons => {
         return respons.data.hits
     })
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import {getPhotos} from "./api.js"
 
 const searchBtn = document.querySelector("button");
+const photoBox = document.querySelector(".gallery")
 
 const createGallery = (event) => {
     event.preventDefault()
@@ -8,13 +9,33 @@ const createGallery = (event) => {
 
     getPhotos(input)
     .then (data => {
-        console.log(data)
-            return data
+        let photoArray = []
+        data.map(element => { 
+            let newPhoto = `
+            <div class="photo-card">
+                <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" />
+                <div class="info">
+                    <p class="info-item">
+                    <b>Likes </b>${element.likes}
+                    </p>
+                    <p class="info-item">
+                    <b>Views </b>${element.views}
+                    </p>
+                    <p class="info-item">
+                    <b>Comments </b>${element.comments}
+                    </p>
+                    <p class="info-item">
+                    <b>Downloads </b>${element.downloads}
+                    </p>
+                </div>
+            </div>`
+            photoArray.push(newPhoto)
+        })
+        photoBox.innerHTML = photoArray.join("")
         })
         .catch (error => {
             console.log(error)
         })
-    
 }
 
 searchBtn.addEventListener("click", createGallery)
